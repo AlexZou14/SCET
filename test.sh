f082a02d64b5ebc2c9cd22f2c79af065536a95a4
AlexZou <2435588629@qq.com>
2022-05-08 22:16:14 +0800

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,6 +1,6 @@
 # Testing
-# PYTHONPATH=$PWD:$PYTHONPATH mim test mmedit /home/alex/Desktop/SCET_mim/config/SCET_x2.py --checkpoint /home/alex/Desktop/SCET_mim/weights/SCETx2.pth --save-path /home/alex/Desktop/SCET_mim/result/Set5x2
+ PYTHONPATH=$PWD:$PYTHONPATH mim test mmedit ./config/SCET_x2.py --checkpoint ./weights/SCETx2.pth --save-path Result
 
-# PYTHONPATH=$PWD:$PYTHONPATH mim test mmedit /home/alex/Desktop/SCET_mim/config/SCET_x3.py --checkpoint /home/alex/Desktop/SCET_mim/weights/SCETx3.pth --save-path /home/alex/Desktop/SCET_mim/result/Set5x3
+# PYTHONPATH=$PWD:$PYTHONPATH mim test mmedit ./config/SCET_x3.py --checkpoint./weights/SCETx3.pth --save-path Result
 
-# PYTHONPATH=$PWD:$PYTHONPATH mim test mmedit /home/alex/Desktop/SCET_mim/config/SCET_x4.py --checkpoint /home/alex/Desktop/SCET_mim/weights/SCETx4.pth --save-path /home/alex/Desktop/SCET_mim/result/Set5x4
+# PYTHONPATH=$PWD:$PYTHONPATH mim test mmedit ./config/SCET_x4.py --checkpoint ./weights/SCETx4.pth --save-path Result
